3c36d2805603ee082c479271b00dde184c01ba19
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-26 17:22:28 -0300

    style: format

--- src/presentation/services/github.service.ts
+++ src/presentation/services/github.service.ts
@@ -4,8 +4,8 @@ export class GithubService {
   constructor() {}
 
   public onStar(payload: GitHubStarPayload): string {
-    const { action, sender, repository } = payload;
+    const { action, sender, repository } = payload
 
-    return `User ${ sender.login } ${ action } star on ${ repository.full_name }`;
+    return `User ${sender.login} ${action} star on ${repository.full_name}`
   }
 }
