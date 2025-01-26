7392d90a386d942747ca8f4f2c4a2ed04e84d0b6
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-26 17:20:56 -0300

    fix: use middleware for json

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { GithubController } from './presentation/github/controller'
 
 function main() {
   const app = express()
+  app.use(express.json())
 
   const controller = new GithubController()
   app.post('/api/github', controller.webhookHandler)
