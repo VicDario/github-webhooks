cfc536130ee85e088f3c512856126680709c7be5
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-26 17:21:57 -0300

    feat: implement service to handler events

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express'
+import { GithubService } from '../services/github.service'
 
 export class GithubController {
-  constructor() {}
+  constructor(
+    private readonly githubService: GithubService = new GithubService()
+  ) {}
 
-  webhookHandler(req: Request, res: Response) {
+  webhookHandler = (req: Request, res: Response) => {
     const githubEvent = req.headers['x-github-event'] ?? 'unknown'
     const payload = req.body
+    let message: string
+    switch (githubEvent) {
+      case 'star':
+        message = this.githubService.onStar(payload)
+        break
+      default:
+        message = `Unhandled event: ${githubEvent}`
+        break
+    }
+    console.log(message)
     res.status(201).json('Webhook received')
   }
 }
